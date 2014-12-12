668b56e667ccda159db158423bdc4288161900fe
Camron Levanger <clevanger@insidesales.com>
2014-12-12 16:43:35 -0700

    made the oh-my-zsh and nodejs fix script ignore errors

--- files/fix_nodejs.sh
+++ files/fix_nodejs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #create a symbolic link from nodejs to nodejs
-ln -s /usr/bin/nodejs /usr/bin/node
-ln -s /usr/bin/nodejs /usr/sbin/node
+ln -s /usr/bin/nodejs /usr/bin/node || echo 'already symlinked /bin/node'
+ln -s /usr/bin/nodejs /usr/sbin/node || echo 'already symlinked /sbin/node'
--- files/install_oh-my-zsh.sh
+++ files/install_oh-my-zsh.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -L http://install.ohmyz.sh | sh
+curl -L http://install.ohmyz.sh | sh || echo 'there was an issue with installing oh-my-zsh'
